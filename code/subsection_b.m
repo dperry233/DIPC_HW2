9b8100d459d2fefe23ba941530f2f290cd5f2290
NogaBar <nogabar14@gmail.com>
2017-05-23 19:48:20 +0300

    אישור הגשה וקובץ zip

--- code/subsection_b.m
+++ code/subsection_b.m
@@ -3,6 +3,7 @@ continuous_approx_delta = 0.001;
 figure;
 grid = 0 : continuous_approx_delta : (1+continuous_approx_delta);
 [x_grid,y_grid] = meshgrid(grid,grid);
+% use of  trigonometric identities
 phi_xy=(A/2)*cos(2*pi*omega_x*(x_grid)-2*pi*omega_y*(y_grid))+(A/2)*cos(2*pi*omega_x*(x_grid)+2*pi*omega_y*(y_grid));
 imshow(phi_xy, [-5000 5000]);
 title('Original Signal');
